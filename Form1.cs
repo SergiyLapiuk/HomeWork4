9db8e0a3955add1306879cd5ca52f952d65fe388
SergiyLapiuk <72142067+SergiyLapiuk@users.noreply.github.com>
2021-10-24 01:20:10 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Triangles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void create_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "Прямокутний")
+            {
+                label2.Text = "Введіть дані для створення прямокутного трикутника:";
+                label3.Text = "Перший катет:";
+                label4.Text = "Другий катет:";
+            }
+            else if(textBox1.Text == "Рівнобедренний" )
+            {
+                label2.Text = "Введіть дані для створення рівнобедренного трикутника:";
+                label3.Text = "Сторона:";
+                label4.Text = "Кут у градусах:";
+            } 
+            else
+            {
+                label2.Text = "Вид трикутника вказаний не правильно!";
+                label3.Text = "";
+                label4.Text = "";
+            }
+        }
+
+        private void area_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "Прямокутний")
+            {
+                RightTriangle triangle = new RightTriangle(Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text));
+                label6.Text = triangle.area().ToString();
+                label5.Text = "Площа:";
+            }
+            else if(textBox1.Text == "Рівнобедренний")
+            {
+                EquilateralTriangle triangle = new EquilateralTriangle(Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text));
+                label6.Text = triangle.area().ToString();
+                label5.Text = "Площа:";
+            } else
+            {
+                label6.Text = "Не правильно вказано вид трикутника!";
+                label5.Text = "";
+            }
+        }
+
+        private void perimeter_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Прямокутний")
+            {
+                RightTriangle triangle = new RightTriangle(Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text));
+                label8.Text = triangle.perimeter().ToString();
+                label7.Text = "Периметр:";
+            }
+            else if (textBox1.Text == "Рівнобедренний")
+            {
+                EquilateralTriangle triangle = new EquilateralTriangle(Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text));
+                label8.Text = triangle.perimeter().ToString();
+                label7.Text = "Периметр:";
+            }
+            else
+            {
+                label8.Text = "Не правильно вказано вид трикутника!";
+                label7.Text = "";
+            }
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Triangles
+{
+    abstract class Triangle
+    {
+        protected double a;
+        protected double b;
+        protected double gamma;
+
+        public Triangle(double a, double b, double gamma)
+        {
+            this.a = a;
+            this.b = b;
+            this.gamma = gamma;
+        }
+        public virtual double area()
+        {
+            return Math.Round((a * b * Math.Sin((Math.PI / 180) * gamma))/2, 2);
+        }
+        public virtual double perimeter()
+        {
+            double c = Math.Sqrt(a * a + b * b - 2 * a * b * Math.Cos((Math.PI / 180) * gamma));
+            return Math.Round(a + b + c, 2);
+        }
+    }
+
+    class RightTriangle : Triangle
+    {
+        public RightTriangle(double side1, double side2) : base(side1, side2, 90)
+        {
+
+        }
+        public override double area()
+        {
+            return Math.Round((a * b) / 2, 2);
+        }
+
+        public override double perimeter()
+        {
+            double c = Math.Sqrt(a * a + b * b);
+            return Math.Round(a + b + c, 2);
+        }
+    }
+
+    class EquilateralTriangle : Triangle
+    {
+        public EquilateralTriangle(double side, double gam) : base(side, side, gam)
+        {
+
+        }
+        public override double area()
+        {
+            return Math.Round((a * a * Math.Sin((Math.PI / 180) * gamma))/2, 2);
+        }
+        public override double perimeter()
+        {
+            return Math.Round(2 * a + Math.Sqrt(2 * a * a - 2 *a*a* Math.Cos((Math.PI / 180) * gamma)), 2);
+        }
+    }
+}
