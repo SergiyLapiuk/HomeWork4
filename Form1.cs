02b4f30fe9b96c4d887bd77b2d4b30ee9fa7da3c
SergiyLapiuk <72142067+SergiyLapiuk@users.noreply.github.com>
2021-10-24 01:18:49 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace figures
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void createFigure_Click(object sender, EventArgs e)
+        {
+            if(nameFigure.Text == "Коло")
+            {
+                label2.Text = "Введіть данні для створення кола";
+                label3.Text = "Радіус:";
+                label4.Text = "";
+                label5.Text = "";
+            }
+            else if(nameFigure.Text == "Квадрат")
+            {
+                label2.Text = "Введіть данні для створення квадрату";
+                label3.Text = "Сторона:";
+                label4.Text = "";
+                label5.Text = "";
+            }
+            else if (nameFigure.Text == "Ромб")
+            {
+                label2.Text = "Введіть данні для створення ромба";
+                label3.Text = "Сторона:";
+                label4.Text = "Кут у градусах:";
+                label5.Text = "";
+            }
+            else if (nameFigure.Text == "Трикутник")
+            {
+                label2.Text = "Введіть данні для створення трикутника";
+                label3.Text = "Сторона 1:"; 
+                label4.Text = "Сторона 2:";
+                label5.Text = "Сторона 3:";
+            }
+            else if (nameFigure.Text == "Прямокутник")
+            {
+                label2.Text = "Введіть данні для створення прямокутника";
+                label3.Text = "Сторона 1:";
+                label4.Text = "Сторона 2:";
+                label5.Text = "";
+            }
+            else 
+            {
+                label2.Text = "Неправильна назва фігури:";
+                label3.Text = "";
+                label4.Text = "";
+                label5.Text = "";
+            }
+        }
+
+        private void area_Click(object sender, EventArgs e)
+        {
+            if (nameFigure.Text == "Коло")
+            {
+                Circle circle = new Circle(Convert.ToDouble(textBox2.Text));
+                label8.Text = circle.determineArea().ToString();
+            }
+            else if (nameFigure.Text == "Квадрат")
+            {
+                Square square = new Square(Convert.ToDouble(textBox2.Text));
+                label8.Text = square.determineArea().ToString();
+            }
+            else if (nameFigure.Text == "Трикутник")
+            {
+                if (Triangle.checkSides(Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox3.Text)))
+                {
+                    Triangle triangle = new Triangle(Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox3.Text));
+                    label8.Text = triangle.determineArea().ToString();
+                }
+                else
+                {
+                    label8.Text = "Такого трикутника не існує!";
+                }
+            }
+            else if (nameFigure.Text == "Ромб")
+            {
+                Diamond diamond = new Diamond(Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox1.Text));
+                label8.Text = diamond.determineArea().ToString();
+            }
+            else if (nameFigure.Text == "Прямокутник")
+            {
+                Rectangle rectangle = new Rectangle(Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox1.Text));
+                label8.Text = rectangle.determineArea().ToString();
+            }
+            else
+            {
+                label8.Text = "Введено неправильну назву фігури!";
+            }
+        }
+
+        private void perimeter_Click(object sender, EventArgs e)
+        {
+            if (nameFigure.Text == "Коло")
+            {
+                Circle circle = new Circle(Convert.ToDouble(textBox2.Text));
+                label9.Text = circle.determinePerimeter().ToString();
+            }
+            else if (nameFigure.Text == "Квадрат")
+            {
+                Square square = new Square(Convert.ToDouble(textBox2.Text));
+                label9.Text = square.determinePerimeter().ToString();
+            }
+            else if (nameFigure.Text == "Трикутник")
+            {
+                if (Triangle.checkSides(Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox3.Text)))
+                {
+                    Triangle triangle = new Triangle(Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox3.Text));
+                    label9.Text = triangle.determinePerimeter().ToString();
+                }
+                else
+                {
+                    label9.Text = "Трикутник не створено!";
+                }
+            }
+            else if (nameFigure.Text == "Ромб")
+            {
+                Diamond diamond = new Diamond(Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox1.Text));
+                label9.Text = diamond.determinePerimeter().ToString();
+            }
+            else if (nameFigure.Text == "Прямокутник")
+            {
+                Rectangle rectangle = new Rectangle(Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox1.Text));
+                label9.Text = rectangle.determinePerimeter().ToString();
+            }
+            else
+            {
+                label9.Text = "Введено неправильну назву фігури!";
+            }
+        }
+    }
+}
---
+++ figure.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace figures
+{
+    abstract class Figure
+    {
+        public abstract double determineArea();
+        public abstract double determinePerimeter();
+    }
+
+    class Circle : Figure
+    {
+        private double radius;
+
+        public Circle(double r)
+        {
+            radius = r;
+        }
+        public override double determineArea()
+        {
+            return Math.Round(Math.PI * radius * radius, 2);
+        }
+        public override double determinePerimeter()
+        {
+            return Math.Round(2 * Math.PI * radius, 2);
+        }
+    }
+
+    class Diamond : Figure
+    {
+        private double side;
+        private double angle;
+
+        public Diamond(double s, double ang)
+        {
+            side = s;
+            angle = ang;
+        }
+
+        public override double determineArea()
+        {
+            return Math.Round(side * side * Math.Sin((Math.PI/180)*angle), 2);
+        }
+        public override double determinePerimeter()
+        {
+            return 4 * side;
+        }
+    }
+
+    class Triangle : Figure
+    {
+        private double side1;
+        private double side2;
+        private double side3;
+
+        public Triangle(double a, double b, double c)
+        {
+            side1 = a;
+            side2 = b;
+            side3 = c;
+        }
+
+        public static bool checkSides(double a, double b, double c)
+        {
+            if (a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override double determineArea()
+        {
+            double p = (side1 + side2 + side3) / 2;
+            return Math.Round(Math.Sqrt(p * (p - side1) * (p - side2) * (p - side3)), 2);
+        }
+        public override double determinePerimeter()
+        {
+            return side1 +  side2 + side3;
+        }
+    }
+
+    class Square : Figure
+    {
+        private double side;
+
+        public Square(double s)
+        {
+            side = s;
+        }
+
+        public override double determineArea()
+        {
+            return side * side;
+        }
+        public override double determinePerimeter()
+        {
+            return 4 * side;
+        }
+    }
+
+    class Rectangle : Figure
+    {
+        private double side1;
+        private double side2;
+
+        public Rectangle(double s1, double s2)
+        {
+            side1 = s1;
+            side2 = s2;
+        }
+
+        public override double determineArea()
+        {
+            return side1 * side2;
+        }
+
+        public override double determinePerimeter()
+        {
+            return 2*(side1 + side2);
+        }
+     }
+}
