6dee20de3937eb05e731b3ed70c9e076f759d903
SergiyLapiuk <72142067+SergiyLapiuk@users.noreply.github.com>
2021-10-24 01:16:26 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(Triangle.checkSides(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text)))
+            {
+                Triangle triangle = new Triangle(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text));
+                label4.Text = "Трикутник створено!";
+            }
+            else
+            {
+                label4.Text = "Трикутник не створено!";
+            }
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Triangle.checkSides(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text)))
+            {
+                Triangle triangle = new Triangle(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text));
+                label5.Text = "Кути визначено:";
+                List<double> angles = triangle.determineAngles();
+                label6.Text = angles[1].ToString();
+                label7.Text = angles[0].ToString();
+                label8.Text = angles[2].ToString();
+            }
+            else
+            {
+                label5.Text = "Трикутник не створено!";
+                label6.Text = "";
+                label7.Text = "";
+                label8.Text = "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Triangle.checkSides(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text)))
+            {
+                Triangle triangle = new Triangle(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text));
+                label13.Text = "Периметр визначено:";
+                label14.Text = triangle.determinePerimeter().ToString();
+            }
+            else
+            {
+                label13.Text = "Трикутник не створено!";
+                label14.Text = "";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(Triangle.checkSides(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text)))
+            {
+                if(Convert.ToDouble(textBox1.Text) == Convert.ToDouble(textBox2.Text) && Convert.ToDouble(textBox2.Text) == Convert.ToDouble(textBox3.Text))
+                {
+                    label15.Text = "Трикутник рівносторонній!";
+                    EquilateralTriangle eTriangle = new EquilateralTriangle(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox3.Text));
+                    label17.Text = eTriangle.determineArea().ToString();
+                }
+                else
+                {
+                    label15.Text = "Трикутник не є рівностороннім!";
+                    label17.Text = "";
+                }
+            }
+            else
+            {
+                label15.Text = "Трикутник не створено!";
+                label17.Text = "";
+            }
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace WinFormsApp1
+{
+    class Triangle
+    {
+        public double side_1;
+        public double side_2;
+        public double side_3;
+
+        public static bool checkSides(double a, double b, double c)
+        {
+            if(a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Triangle(double a, double b, double c)
+        {
+            try
+            {
+                if(!checkSides(a, b, c))
+                {
+                    throw new Exception();
+                }
+                else
+                {
+                    side_1 = a;
+                    side_2 = b;
+                    side_3 = c;
+                }
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+        public List<double> determineAngles()
+        {
+            List<double> angles = new List<double>();
+            double angle_1 = Math.Round(Math.Acos((side_1 * side_1 + side_2 * side_2 - side_3 * side_3) / (2 * side_1 * side_2))*(180/Math.PI), 2);
+            angles.Add(angle_1);
+            double angle_2 = Math.Round(Math.Acos((side_1 * side_1 + side_3 * side_3 - side_2 * side_2) / (2 * side_1 * side_3)) * (180 / Math.PI), 2);
+            angles.Add(angle_2);
+            double angle_3 = Math.Round(Math.Acos((side_3 * side_3 + side_2 * side_2 - side_1 * side_1) / (2 * side_3 * side_2)) * (180 / Math.PI), 2);
+            angles.Add(angle_3);
+            return angles;
+        }
+
+        public double determinePerimeter()
+        {
+            return Math.Round(side_1 + side_2 + side_3, 2);
+        }
+    }
+
+    class EquilateralTriangle : Triangle
+    {
+        public EquilateralTriangle (double a, double b, double c) : base (a, b, c)
+        {
+
+        }
+
+        public double determineArea()
+        {
+            return Math.Round((side_1 * side_1 * Math.Sqrt(3)) / 4, 2);
+        }
+    }
+}
